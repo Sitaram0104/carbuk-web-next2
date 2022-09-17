a6c54f6019ce64185d5b45d7f38d0bdbc84e2307
Sitaram0104 <meenasitu0104@gmail.com>
2022-09-18 02:15:20 +0530

    gps

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,16 +1,18 @@
 import tw from "tailwind-styled-components";
 import mapboxgl from "!mapbox-gl";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic2l0YXJhbTAxMDQiLCJhIjoiY2wyZDVqaTMxMGV4YjNpbXY1a3M4NHptbyJ9.nWEWJk3WUoyK7Es2jMV_3Q";
 
 const Map = ({ pickupCoordinates, dropoffCoordinates }) => {
+  const [gps, setGps] = useState([80, 26.9]);
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
-      center: [76, 26.9],
+      center: gps,
       zoom: 7,
     });
 
@@ -23,11 +25,22 @@ const Map = ({ pickupCoordinates, dropoffCoordinates }) => {
     if (pickupCoordinates && dropoffCoordinates) {
       map.fitBounds([pickupCoordinates, dropoffCoordinates], { padding: 60 });
     }
-  }, [pickupCoordinates, dropoffCoordinates]);
+  }, [pickupCoordinates, dropoffCoordinates, gps]);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((p) =>
+        setGps([p.coords.longitude, p.coords.latitude])
+      );
+    } else {
+      setGps([77, 26.9]);
+    }
+  }, []);
 
   const addToMap = (map, coord) => {
     const marker1 = new mapboxgl.Marker().setLngLat(coord).addTo(map);
   };
+  console.log(gps);
 
   return <Wrapper id="map" />;
 };
